e409aeab11b8c0660927e6f1db66f7f4c4442f8e
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-04-17 20:56:42 +0430

    update routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Circle } from '../Circle';
+import { Logo } from '../Logo';
 import { Navbar } from '../Navbar';
+import { Pentagon } from '../Pentagon';
+import './styles/MainStyles.css';
 
 export function App() {
 	return (
 		<div>
-			<Navbar />
 			<BrowserRouter>
+				<Navbar />
 				<Switch>
 					<Route exact path="/" render={(routeProps) => <Circle routeProps={routeProps} />} />
+					<Route exact path="/pentagon" render={(routeProps) => <Pentagon routeProps={routeProps} />} />
+					<Route exact path="/logo" render={(routeProps) => <Logo routeProps={routeProps} />} />
 				</Switch>
 			</BrowserRouter>
 		</div>
